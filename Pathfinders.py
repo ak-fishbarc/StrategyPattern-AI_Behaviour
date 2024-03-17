467c37a29478cf839e95a1ae4739944cf8488fa2
ak-fishbarc <rybarczyk.ak@gmail.com>
2024-03-17 15:24:18 +0000

    Added tests for pathfinding with obstacles. Improved pathfinding function to pass the test. Added functions to Pathfinders, functions used by pathfining algorithm; function to check for empty positions; functions for calculating manhattan distance and euclidean distance. Function to track the path.

--- Pathfinders.py
+++ Pathfinders.py
@@ -7,7 +7,7 @@ class Pathfinders(ABC):
     def find_path(self, level_map: list, start: tuple, end: tuple):
         pass
 
-    # Check around on the level_map if there are any empty positions around start.
+    # Check around start position on the level_map if there are any empty positions.
     def check_if_position_is_empty(self, level_map: list, start: tuple):
         empty_positions = []
 
@@ -34,35 +34,96 @@ class Pathfinders(ABC):
         result = abs(position1[0] - position2[0]) + abs(position1[1] - position2[1])
         return result
 
+    def calculate_euclidean_distance(self, position1: tuple, position2: tuple):
+        result = ((position1[0] - position2[0])**2 + (position1[1] - position2[1])**2)**0.5
+        return result
 
-""" 
-    This class moves towards the target. In steps:
-    1. Set the pointer to current position.
-    2. Loop until the target is reached. If the target is reached go to Step 7.
-    3. Look for empty positions around current position.
-    4. For any empty position check if it's closer to the target than the current position.
-    5. If it's closer, set that closer position as the new pointer.
-    6. Go back to point 2.
-    7. Return path.
-"""
+    def track_path(self, level_map, start: tuple, end: tuple):
+        path = [start]
+        pointer = start
+        nodes = {}
+        alternative_nodes = []
+        current_node = None
+        counter = 0
+        paths = {}
+        empty_positions = self.check_if_position_is_empty(level_map, start)
+
+        while pointer != end or len(alternative_nodes) > 0:
+
+            if pointer == end and len(alternative_nodes) > 0:
+                path = [start]
+                pointer = alternative_nodes.pop()
+                path.append(pointer)
+                empty_positions = self.check_if_position_is_empty(level_map, pointer)
+
+            for position in empty_positions:
+                result_position = self.calculate_euclidean_distance(position, end)
+                nodes[position] = result_position
+
+            for node in nodes:
+                if current_node is None and node not in path:
+                    current_node = node
+
+                if current_node is not None and current_node != node and nodes[current_node] > nodes[node] and node not in path:
+                    current_node = node
+                elif current_node is not None and current_node != node and nodes[current_node] == nodes[node] and node not in path:
+                    alternative_nodes.append(node)
+
+            if current_node is not None and current_node not in path:
+                path.append(current_node)
+                pointer = current_node
+
+            if current_node is None:
+                return paths
+            current_node = None
+            nodes = {}
+            empty_positions = self.check_if_position_is_empty(level_map, pointer)
+
+            if pointer == end:
+                paths[counter] = path
+                counter += 1
+
+        return paths
 
 
 class GoTo(Pathfinders):
     def find_path(self, level_map: list, start: tuple, end: tuple):
-        path = []
-        pointer = start
-        while self.calculate_manhattan_distance(pointer, end) >= 1:
-            empty_positions = self.check_if_position_is_empty(level_map, pointer)
+        paths = self.track_path(level_map, start, end)
+        combined_paths = {}
+        counter = 0
+        for key in paths:
+            counter = key
+        for path in paths:
+            for position in paths[path]:
+                counter += 1
+                combine_paths = []
+                check_path_to_start = self.track_path(level_map, position, start)
+                check_path_to_end = self.track_path(level_map, position, end)
 
-            for position in empty_positions:
-                result = self.calculate_manhattan_distance(position, end)
-                check_pointer = self.calculate_manhattan_distance(pointer, end)
-                if check_pointer > result:
-                    pointer = position
+                for key in check_path_to_start:
+                    for value in reversed(check_path_to_start[key]):
+                        if value not in combine_paths:
+                            combine_paths.append(value)
 
-            path.append(pointer)
+                for key in check_path_to_end:
+                    for value in check_path_to_end[key]:
+                        if value not in combine_paths:
+                            combine_paths.append(value)
 
-        return path
+                if len(combine_paths) > 0 and start in combine_paths and end in combine_paths:
+                    combined_paths[counter] = combine_paths
+
+        for key in combined_paths:
+            paths[key] = combined_paths[key]
+
+        current_path = None
+
+        for path in paths:
+            if current_path is None:
+                current_path = path
+            if current_path != path and len(paths[current_path]) > len(paths[path]):
+                current_path = path
+        return paths[current_path]
 
 
 class KeepDistance(Pathfinders):
--- Tests/Test_Pathfinders.py
+++ Tests/Test_Pathfinders.py
@@ -1,8 +1,23 @@
 import unittest
-from Tests import ToolsForTesting as TFT
 from Pathfinders import GoTo
 
-level_map = [[0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0]]
+level_map = [[0, 0, 0, 0, 0],
+             [0, 0, 0, 0, 0],
+             [0, 0, 0, 0, 0],
+             [0, 0, 0, 0, 0],
+             [0, 0, 0, 0, 0]]
+
+level_map_with_obstacle = [[0, 0, 0, 5, 0],
+                           [0, 0, 0, 5, 0],
+                           [0, 0, 0, 5, 0],
+                           [0, 0, 0, 5, 0],
+                           [0, 0, 0, 0, 0]]
+
+level_map_with_obstacle2 = [[0, 0, 0, 0, 0],
+                           [0, 0, 0, 5, 0],
+                           [0, 0, 0, 5, 0],
+                           [0, 0, 0, 5, 0],
+                           [0, 0, 0, 5, 0]]
 
 
 class TestPathfinders(unittest.TestCase):
@@ -10,17 +25,22 @@ class TestPathfinders(unittest.TestCase):
     def setUp(self):
         self.go_to = GoTo()
         self.start = (2, 2)
-        self.end = (4, 4)
+        self.end = (2, 4)
 
     def test_goto_no_obstacles(self):
         found_path = self.go_to.find_path(level_map, self.start, self.end)
-        self.assertEqual(len(found_path), 4)
+        self.assertGreater(len(found_path), 0)
         for position in found_path:
-            self.assertLess(TFT.calculate_manhattan_distance(position, self.end),
-                            TFT.calculate_manhattan_distance(self.start, self.end))
+            self.assertLessEqual(self.go_to.calculate_euclidean_distance(position, self.end),
+                                 self.go_to.calculate_euclidean_distance(self.start, self.end))
 
     def test_goto_obstacle(self):
-        pass
+        valid_path = [(2, 2), (3, 2), (4, 2), (4, 3), (4, 4), (3, 4), (2, 4)]
+        valid_path2 = [(2, 2), (1, 2), (0, 2), (0, 3), (0, 4), (1, 4), (2, 4)]
+        found_path = self.go_to.find_path(level_map_with_obstacle, self.start, self.end)
+        found_path2 = self.go_to.find_path(level_map_with_obstacle2, self.start, self.end)
+        self.assertListEqual(valid_path, found_path)
+        self.assertListEqual(valid_path2, found_path2)
 
     def test_goto_no_valid_path(self):
         pass
