26094108a109dd27d6b503dbb2751535fc4fb4c9
ak-fishbarc <rybarczyk.ak@gmail.com>
2024-03-19 20:54:31 +0000

    Added implementation for Test_Pathfinders_GoAway class test_go_away_no_obstacles method.

--- Tests/Test_Pathfinders_GoAway.py
+++ Tests/Test_Pathfinders_GoAway.py
@@ -16,7 +16,15 @@ class TestPathfindersGoAway(unittest.TestCase):
         self.go_away_from = (2, 4)
 
     def test_go_away_no_obstacles(self):
-        pass
+        escape_path = self.go_away.find_path(level_map, self.my_position, self.go_away_from)
+        distance_counter = 0
+        for point in escape_path:
+            distance_counter += 1
+            """ Avoid measuring distance from starting position.
+                Make sure that with each step it's further away from the target on an empty map """
+            if point != self.my_position:
+                measure_distance = self.go_away.calculate_manhattan_distance(point, self.go_away_from)
+                self.assertEqual(measure_distance, distance_counter)
 
     def test_go_away_obstacle(self):
         pass
